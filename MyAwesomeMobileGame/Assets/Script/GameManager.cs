eaaf246f6429d1c4b341b82fd1caf8a362b5527b
BeiNicolas <bei.nicolas8484@gmail.com>
2024-11-06 14:23:54 +0100

    SAVEFirstCycleGame

--- MyAwesomeMobileGame/Assets/Script/GameManager.cs
+++ MyAwesomeMobileGame/Assets/Script/GameManager.cs
@@ -2,16 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public PlayerShootSystem playerShootSystem; // Référence au script de tir du joueur
     public TextMeshProUGUI countdownText; // Le texte du compte à rebours
+    public GameObject gameOverPanel; // Référence au panneau de fin de jeu
+    public LayerMask enemyLayer; // Layer des ennemis
+
+    private bool gameOver = false; // Indicateur pour vérifier si le jeu est terminé
 
     void Start()
     {
         DisableShooting();
+        gameOverPanel.SetActive(false); // Désactiver le panneau de fin de jeu au début
         StartCoroutine(StartGameSequence());
+    }
+
+    void Update()
+    {
+        if (!gameOver)
+        {
+            CheckGameOver();
+        }
     }
 
     void DisableShooting()
@@ -40,5 +54,43 @@ public class GameManager : MonoBehaviour
         countdownText.gameObject.SetActive(false);
 
         EnableShooting();
+    }
+
+    void CheckGameOver()
+    {
+        bool enemyExists = false;
+        foreach (var t in FindObjectsOfType<Transform>())
+        {
+            if (((1 << t.gameObject.layer) & enemyLayer) != 0)
+            {
+                enemyExists = true;
+                break;
+            }
+        }
+
+        if (!enemyExists)
+        {
+            EndGame();
+        }
+    }
+
+    void EndGame()
+    {
+        gameOverPanel.SetActive(true); // Activer le panneau de fin de jeu
+        DisableShooting(); // Désactiver les tirs du joueur
+        gameOver = true; // Indiquer que le jeu est terminé
+    }
+
+    public void ReplayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // Recharger la scène actuelle
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(); // Quitter le jeu
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // Arrêter le mode de jeu dans l'éditeur
+#endif
     }
 }
